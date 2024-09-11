17f2ebd124ad116e23576b9b2983b5ff4bd2a417
Murzuqisah <amosjoel229@gmail.com>
2024-09-11 04:40:18 +0300

    refactor: restructure code base

--- main.go
+++ main.go
@@ -1,79 +1,36 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"text/template"
-	"time"
+	"os"
+
+	"search/functions"
 )
 
-type Artist struct {
-	ID           int      `json:"id"`
-	Image        string   `json:"image"`
-	Name         string   `json:"name"`
-	Members      []string `json:"members"`
-	CreationDate int      `json:"creationDate"`
-	FirstAlbum   string   `json:"firstAlbum"`
-	Locations    string   `json:"locations"`
-	ConcertDates string   `json:"concertDates"`
-	Relations    string   `json:"relations"`
-}
-
-type Artists []Artist
-
-var data Artists
-
-func fetchJSON() {
-	client := &http.Client{
-		Timeout: 10 * time.Second, // timeout the request if response is delayed
-	}
-	resp, err := client.Get("https://groupietrackers.herokuapp.com/api/artists")
-	if err != nil {
-		fmt.Println("error fetching JSON")
-		return
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Error : Received status code %d\n", resp.StatusCode)
-		return
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error: Reading from repsonse body")
-		return
-	}
-
-	err = json.Unmarshal(body, &data)
-	if err != nil {
-		fmt.Println("Error unmarshalling JSON body", err)
-		fmt.Println("body: ", string(body))
-		return
-	}
-}
-
-func artistHandler(w http.ResponseWriter, r *http.Request) {
-	fetchJSON()
-	tmpl := template.Must(template.ParseFiles("index.html"))
-	tmpl.Execute(w, data)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is home")
-}
-
-func handlerFunctions() {
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/artists", artistHandler)
-	fmt.Println("Server is running...")
-	log.Fatal(http.ListenAndServe(":8001", nil))
-}
-
 func main() {
-	handlerFunctions()
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go [PORT]")
+		return
+	}
+
+	port := ":" + os.Args[1]
+	functions.LoadData()
+	http.HandleFunc("/", functions.Index)
+	http.HandleFunc("/artists", functions.Artists)
+	http.HandleFunc("/about", functions.About)
+	http.HandleFunc("/artists/", functions.ArtistDetail)
+	http.HandleFunc("/concerts", functions.Concerts)
+
+	staticDir := "./static/"
+	staticURL := "/static/"
+	fileServer := http.FileServer(http.Dir(staticDir))
+	http.Handle(staticURL, http.StripPrefix(staticURL, fileServer))
+
+	log.Printf("Server started at http://localhost%s\n", port)
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
 }
