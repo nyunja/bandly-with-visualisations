200f22b1e1e436c172da7d6d758eeb09bb4effd4
johnotieno0 <poptiff@gmail.com>
2024-10-08 12:59:22 +0300

    refactor: endpoint handler for /about to take a handler that takes another handler with an additional path argument

--- main.go
+++ main.go
@@ -25,7 +25,9 @@ func main() {
 		functions.Index(w, r, "templates/index.html")
 	})
 	// http.HandleFunc("/artists", functions.Artists)
-	http.HandleFunc("/about", functions.About)
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request){
+		functions.About(w, r, "templates/about.html")
+	})
 	http.HandleFunc("/artists/", func(w http.ResponseWriter, r *http.Request){
 		functions.ArtistDetail(w, r, "templates/band.html")
 	})
