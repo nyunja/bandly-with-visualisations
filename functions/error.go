c015553dd320e8772d2081b4179dadb9f3ae58f7
johnotieno0 <poptiff@gmail.com>
2024-10-08 16:56:37 +0300

    fix: remove error log from terminal

--- functions/error.go
+++ functions/error.go
@@ -15,7 +15,7 @@ type Message struct {
 func ServeError(w http.ResponseWriter, errVal string, statusCode int, path string) {
 	tmpl, err := template.ParseFiles(path)
 	if err != nil {
-		log.Printf("error parsing files: %v\n", err)
+		// log.Printf("error parsing files: %v\n", err)
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
 		return
 	}
